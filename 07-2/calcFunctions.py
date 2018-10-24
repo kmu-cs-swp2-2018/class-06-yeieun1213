a98e3733d40f6d59f8e7fe970036d9c7969b8ca6
yeieun1213 <yeieun1213@kookmin.ac.kr>
2018-10-25 00:38:57 +0900

    코드 정리

---
+++ 07-2/calcFunctions.py
@@ -0,0 +1,39 @@
+from math import factorial as fact
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+def decToRoman(numStr):
+    return 'dec -> Roman'
+
+def calc(i, numStr):
+    list = [factorial(numStr), decToBin(numStr), binToDec(numStr), decToRoman(numStr)]
+    answer = list[i]
+    return answer
+
+if __name__ == '__main__':
+    print(factorial(5),
+    factorial(0),
+    factorial(-1),
+    factorial(3.5))
